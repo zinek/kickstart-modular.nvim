2c7106a0a1f7f3b4dd0c5792d888dc83e3f56d30
Marcin Zinkiewicz <marcin.zinkiewicz@gmail.com>
2024-05-07 16:52:00 +0200

    lualine

--- lua/options.lua
+++ lua/options.lua
@@ -57,6 +57,6 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 4
 
 -- vim: ts=2 sts=2 sw=2 et

9c1e3824e4ab393a3705ef567b0cbc59829ce7e0
Marcin Zinkiewicz <marcin.zinkiewicz@gmail.com>
2024-05-08 19:34:20 +0200

    obsidian

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -55,6 +55,12 @@ return {
       mode = '',
       desc = '[O]bsidian [G]rep',
     },
+    {
+      '<leader>oe',
+      '<cmd>ObsidianExtractNote<CR>',
+      mode = 'v',
+      desc = '[O]bsidian [E]xtract Note',
+    },
   },
   dependencies = {
     -- Required.
@@ -74,6 +80,8 @@ return {
       },
     },
 
+    notes_subdir = 'notes',
+
     daily_notes = {
       -- Optional, if you keep daily notes in a separate directory.
       folder = 'dailies',
@@ -92,6 +100,35 @@ return {
       -- Trigger completion at 2 chars.
       min_chars = 2,
     },
+
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ''
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.time()) .. '-' .. suffix
+    end,
+
+    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+    -- URL it will be ignored but you can customize this behavior here.
+    ---@param url string
+    follow_url_func = function(url)
+      -- Open the URL in the default web browser.
+      -- vim.fn.jobstart { 'open', url } -- Mac OS
+      vim.fn.jobstart { 'wsl-open', url } -- linux
+    end,
 
     -- see below for full list of options 👇
   },

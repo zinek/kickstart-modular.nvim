de4934475659c7707b373f350274c6615b21de64
Marcin Zinkiewicz <marcin.zinkiewicz@gmail.com>
2024-07-04 13:09:09 +0200

    nvim-rest-console config

--- lua/custom/plugins/nvim-rest-console.lua
+++ lua/custom/plugins/nvim-rest-console.lua
@@ -5,8 +5,14 @@ return {
     -- vim.g.vrc_set_default_mapping = 0
     vim.g.vrc_response_default_content_type = 'application/json'
     vim.g.vrc_output_buffer_name = '_OUTPUT_.json'
-    vim.g.vrc_aut_format_response_patters = {
+    vim.g.vrc_auto_format_response_enabled = 1
+    vim.g.vrc_auto_format_response_patterns = {
       json = 'jq',
     }
+    -- vim.g.vrc_curl_opts = {
+    --   ['--silent'] = '',
+    --   ['--insecure'] = '',
+    --   ['-i'] = '',
+    -- }
   end,
 }

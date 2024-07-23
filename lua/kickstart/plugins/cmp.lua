5c376068f98391179afa422598c3d080bc618520
Marcin Zinkiewicz <marcin.zinkiewicz@gmail.com>
2024-07-23 16:10:23 +0200

    cmp - <tab> to expand

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -67,6 +67,7 @@ return {
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
           ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<Tab>'] = cmp.mapping.confirm { select = true },
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display

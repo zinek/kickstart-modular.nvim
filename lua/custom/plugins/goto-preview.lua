18410679069f56682037c17ab7f410c6dda32353
Marcin Zinkiewicz <marcin.zinkiewicz@gmail.com>
2024-10-23 10:43:13 +0200

    goto-preview toggleterm gp

---
+++ lua/custom/plugins/goto-preview.lua
@@ -0,0 +1,45 @@
+return {
+    "rmagatti/goto-preview",
+    -- event = "VeryLazy",
+    keys = {
+
+        {
+            "<leader>pd",
+            function()
+                require("goto-preview").goto_preview_definition()
+            end,
+            { desc = "Preview Definition", silent = true },
+        },
+        {
+            "<leader>pt",
+            function()
+                require("goto-preview").goto_preview_type_definition()
+            end,
+            { desc = "Preview Type Definition", silent = true },
+        },
+        {
+            "<leader>pi",
+            function()
+                require("goto-preview").goto_preview_implementation()
+            end,
+            { desc = "Preview Implementation", silent = true },
+        },
+        {
+            "<leader>pr",
+            function()
+                require("goto-preview").goto_preview_references()
+            end,
+            { desc = "Preview References", silent = true },
+        },
+        {
+            "<leader>pc",
+            function()
+                require("goto-preview").close_all_win()
+            end,
+            { desc = "Close Previews", silent = true },
+        },
+    },
+    config = function()
+        require("goto-preview").setup()
+    end,
+}
--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -40,7 +40,7 @@ return {
 
       -- Remap adding surrounding to Visual mode selection
       vim.keymap.del('x', 'ys')
-      vim.keymap.set('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
+      vim.keymap.set('x', 'gS', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
 
       -- Make special mapping for "add surrounding for line"
       vim.keymap.set('n', 'yss', 'ys_', { remap = true })
